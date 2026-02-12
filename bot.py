afc1710ded60f72f3fded06e1679d81ae74acf85
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-12 14:47:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "8430851059:AAFeU-6EGQYjQsv8DqnV0G8gwrOJdcyHjkw"
+TOKEN = os.getenv("BOT_TOKEN")
+
 
 async def debug_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("CHAT ID:", update.effective_chat.id)
-    print("MESSAGE:", update.message.text)
 
-async def main():
+
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    # Ловим ВСЕ сообщения
     app.add_handler(MessageHandler(filters.ALL, debug_chat))
 
     print("Bot started...")
-    await app.run_polling()
+    app.run_polling()
+
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
