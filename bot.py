c9d6ab020c8ad2ea9f1776735701a080b1a62fcc
trofkillaz <134133199+trofkillaz@users.noreply.github.com>
2026-02-17 22:10:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 from aiogram.utils import executor
 import aiohttp
